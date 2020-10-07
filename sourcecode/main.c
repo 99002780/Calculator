dcca5a27848751de276d2a14fa9640f99b3a488e
99002780 <72479185+99002780@users.noreply.github.com>
2020-10-07 22:48:05 +0530

    Create main.c

---
+++ sourcecode/main.c
@@ -0,0 +1,39 @@
+void addition();
+void subtraction();
+void multiplication();
+void division();
+int factorial();
+int power();
+void operations();
+
+int main()
+{
+    char operator;
+
+	int start;
+	operations();
+    printf("\n");
+    printf("\n Please Enter an Operator : ");
+    scanf("%c", &operator);
+    switch(operator)
+        {
+            case '+': addition();
+                      break;
+
+            case '-': subtraction();
+                      break;
+
+            case '*': multiplication();
+                      break;
+
+            case '/': division();
+                      break;
+			case '!': factorial();
+                      break;
+            case '^': power();
+                      break;
+            case 'q': exit(0);
+                      break;
+        }
+	return 0;
+}
